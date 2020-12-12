c1cecbe0d71e21d4564e70d735098d365dbed567
Gabriel Dantas <gabrielmd123@gmail.com>
2020-12-12 18:19:07 -0300

    simple beginnings

---
+++ madlibs/index.py
@@ -0,0 +1,17 @@
+# youtuber = "kkk"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+print("Welcome to my small madlib! Let's start by choosing a few words...")
+
+adj = input("An adjective: ")
+verb1 = input("A verb: ")
+verb2 = input("Another verb: ")
+famous_person = input("A famous person: ")
+
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hidrated and {verb2} like you are {famous_person}!" 
+
+print(madlib)
